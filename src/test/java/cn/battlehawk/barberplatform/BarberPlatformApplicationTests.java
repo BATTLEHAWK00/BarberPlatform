b442cef054de4d456f298612716ed82cae0b3e5d
BATTLEHAWK00 <battlehawk0_0@163.com>
2021-11-26 17:08:09 +0800

    修复测试无法通过的情况

--- src/test/java/cn/battlehawk/barberplatform/BarberPlatformApplicationTests.java
+++ src/test/java/cn/battlehawk/barberplatform/BarberPlatformApplicationTests.java
@@ -1,14 +1,12 @@
 package cn.battlehawk.barberplatform;
 
 import cn.battlehawk233.barberplatform.dao.SettingMapper;
-import org.assertj.core.api.Fail;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {cn.battlehawk233.barberplatform.BarberPlatformApplication.class})
+@SpringBootTest(classes = {cn.battlehawk233.barberplatform.BarberPlatformApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BarberPlatformApplicationTests {
     @Autowired
     public SettingMapper settingsMapper;
