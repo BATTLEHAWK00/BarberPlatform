0b4eb8df5ffadef5c24fcd3855a6ab4c74a9179a
BATTLEHAWK <154075059@qq.com>
2022-09-24 23:15:56 +0800

    chore: unversioned files

---
+++ src/test/java/cn/battlehawk233/barbershop/BarbershopApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.battlehawk233.barbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
